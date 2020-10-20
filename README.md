b9238df2c689899103cca828167a666b575eabfa
isolate-spark <giordanotemple@gmail.com>
2020-10-20 11:33:44 -0400

    created flight ~ airport associations

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 1. add devise gem and configurations
 
-2.
+2. Added User, Flight, and Airport models
+
+3. Seeded Airport model
+
+4. created associations
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,6 @@
 class Flight < ApplicationRecord
+
+  belongs_to :departure_airport, class_name: 'Airport'
+  belongs_to :arrival_airport, class_game: 'Airport'
+
 end
